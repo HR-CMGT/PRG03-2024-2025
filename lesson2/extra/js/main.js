33089c139471f46682e3c6e31fb5b7192393c3a2
antwanvdm <antwanvdm@gmail.com>
2025-02-13 15:24:27 +0100

    Lesson 2 code

---
+++ lesson2/extra/js/main.js
@@ -0,0 +1,56 @@
+function showDate() {
+  const date = new Date();
+  const month = date.toLocaleString('nl-NL', {month: 'long'});
+  console.log(`${date.getDate()} ${month} ${date.getFullYear()}`);
+}
+
+function reverseString(text) {
+  console.log(text.split('').reverse().join(''));
+}
+
+function isTheAnswerToLife(numbers) {
+  if (numbers.includes(42)) {
+    return true;
+  }
+
+  let sum = 0;
+  for (let number of numbers) {
+    sum += number;
+  }
+
+  return sum === 42;
+}
+
+function daysUntilMyBirthday(date) {
+  const today = new Date();
+  const birthday = new Date(date);
+
+  const timeDifference = birthday - today;
+  return Math.ceil(timeDifference / (1000 * 60 * 60 * 24));
+}
+
+
+
+
+
+
+showDate(); //13 februari 2025
+reverseString('hello'); //olleh
+reverseString('antwan'); //nawtna
+console.log(isTheAnswerToLife([3, 15, 23, 1])); //true
+console.log(isTheAnswerToLife([3, 15, 23, 2])); //false
+console.log(isTheAnswerToLife([3, 15, 23, 1000, 42])); //true
+console.log(daysUntilMyBirthday('2025-05-10')); //86
+
+
+
+
+
+
+
+
+
+
+// taal formatting
+const df = new Intl.RelativeTimeFormat('nl-NL', {style: 'long'});
+console.log(df.format(daysUntilMyBirthday('2025-05-10'), 'days')); // "over X dagen"
